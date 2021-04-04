be7f57d8e258d2e2d97cb70ec8b31910100e722a
Kazi <Kazi Ejajul>
2021-04-04 15:34:12 +0600

    --- Delete a node by given position ---
    	modified:   delete_a_linked_list_node_at_a_given_position.py

--- delete_a_linked_list_node_at_a_given_position.py
+++ delete_a_linked_list_node_at_a_given_position.py
@@ -3,7 +3,8 @@ class Node:
         self.data = data
         self.next = None
 
-class LinkList:
+
+class LinkedList:
     def __init__(self):
         self.head = None
 
@@ -11,3 +12,49 @@ class LinkList:
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
+
+    def deleteNode(self, position):
+
+        if self.head == None:
+            return
+
+        current = self.head
+
+        if position == 0:
+            self.head = current.next
+            current = None
+            return
+
+        for i in range(position -1):
+            current = current.next
+            if current is None:
+                break
+
+        if current is None:
+            return
+        if current.next is None:
+            return
+
+        next = current.next.next
+        current.next = None
+        current.next = next
+
+    def printList(self):
+        current = self.head
+        while current:
+            print(" %d " % current.data)
+            current = current.next
+
+
+llist = LinkedList()
+llist.push(7)
+llist.push(1)
+llist.push(3)
+llist.push(2)
+llist.push(8)
+
+print("Created Linked List: ")
+llist.printList()
+llist.deleteNode(4)
+print("\nLinked List after Deletion at position 4: ")
+llist.printList()
